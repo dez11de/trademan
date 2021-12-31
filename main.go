64812ab26d011290bd6a5e9ebc25ae3633384818
dez11de <dennis@ganzeinfach.nl>
2021-12-31 21:11:49 +0100

    Switched UI library/framework
    Switched from github.com/rivo/tview to
    github.com/charmbracelet/bubbletea. This makes things look more modern
    and fresher.
    It requires more code but is actively maintained and just looks better.

--- main.go
+++ main.go
@@ -2,61 +2,21 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
+	"os"
+	"time"
 
-	"github.com/rivo/tview"
+	tea "github.com/charmbracelet/bubbletea"
 )
 
-type UI struct {
-	app           *tview.Application
-	loggingWindow *tview.TextView
-	positionTable *tview.Table
-	positionForm  *tview.Form
-	flex          *tview.Flex
-	pages         *tview.Pages
-}
-
 func main() {
-	ui := SetupMain()
+	rand.Seed(time.Now().UTC().UnixNano())
 
 	db := NewDB()
-	dberr := db.Connect()
-	if dberr != nil {
-		fmt.Fprintf(ui.loggingWindow, "Database error: %+v\n", dberr)
-	}
+	db.Connect()
 
-	t := Position{
-		Symbol:          "ADAUSDT",
-		Side:            planned,
-		Risk:            0.5,
-		Size:            123.4,
-		EntryPrice:      1.23,
-		HardStopLoss:    1.22,
-		Notes:           "Dit is maar gewoon een test.\nIk heb nog niet echt een plan.\n",
-		TradingViewPlan: "https://tradingview.com/1234asfdf",
-		RewardRiskRatio: 9.06,
-	}
-
-	t.TradeID, dberr = db.AddPosition(t)
-
-	o := Order{
-		TradeID:           t.TradeID,
-		ExchangeOrderID:   "somestring",
-		Status:            planned,
-		OrderType:         TakeProfit,
-		Quantity:          33.3,
-		TakeProfitTrigger: 1.29,
-		Price:             1.30,
-	}
-
-	o.OrderID, dberr = db.AddOrder(o)
-
-	if dberr != nil {
-		fmt.Printf("Error adding order %v", dberr)
-	}
-
-	db.AddLog(t.TradeID, user, "First test log.")
-
-	if err := ui.app.Run(); err != nil {
-		panic(err)
+	if err := tea.NewProgram(newModel(db), tea.WithAltScreen()).Start(); err != nil {
+		fmt.Println("Error running program:", err)
+		os.Exit(1)
 	}
 }

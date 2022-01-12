e2be13640672fe8966854043abff915900c2b320
dez11de <dennis@ganzeinfach.nl>
2022-01-12 14:00:50 +0100

    Refactor Position to use decimal.Decimal instead of float64
    This prevents rounding errors.

--- position.go
+++ position.go
@@ -1,21 +1,21 @@
 package main
 
-import "time"
+import (
+	"time"
+
+	"github.com/shopspring/decimal"
+)
 
 type Position struct {
 	PositionID      int64
-	Status          Status
 	PairID          int64
-	Pair            string
-	Size            float64 `json:"size"`
-	Side            Status  `json:"side"`
+	Status          Status
+	Side            Side `json:"side"`
 	Risk            float64
-	EntryPrice      float64 `json:"entry_price"`
-	HardStopLoss    float64 `json:"stop_loss"`
 	Notes           string
 	TradingViewPlan string
 	RewardRiskRatio float64
-	Profit          float64 `json:"realised_pnl"`
+	Profit          decimal.Decimal `json:"realised_pnl"`
 	EntryTime       time.Time
 	ModifyTime      time.Time
 }

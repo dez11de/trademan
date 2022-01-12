05779a7a9052f19f6ab1174ffe421710561ad33b
dez11de <dennis@ganzeinfach.nl>
2022-01-12 13:58:39 +0100

    Read all Position fields properly

--- db.position.go
+++ db.position.go
@@ -7,12 +7,12 @@ import (
 )
 
 func (db *Database) PrepareAddPositionStatement() (err error) {
-	db.addPositionStatement, err = db.database.Prepare(fmt.Sprintf("INSERT %s SET PairID=?, Status=?, Risk=?, `Size`=?, EntryPrice=?, HardStopLoss=?, Notes=?, TradingViewPlan=?, RewardRiskRatio=?, Profit=?", db.positionTableName))
+	db.addPositionStatement, err = db.database.Prepare(fmt.Sprintf("INSERT %s SET PairID=?, Status=?, Side=?, Risk=?, Notes=?, TradingViewPlan=?, RewardRiskRatio=?, Profit=?", db.positionTableName))
 	return err
 }
 
 func (db *Database) AddPosition(p Position) (TradeID int64, err error) {
-	result, err := db.addPositionStatement.Exec(p.PositionID, p.Side.String(), p.Risk, p.Size, p.EntryPrice, p.HardStopLoss, p.Notes, p.TradingViewPlan, p.RewardRiskRatio, p.Profit)
+	result, err := db.addPositionStatement.Exec(p.PositionID, p.Status.String(), p.Side.String(), p.Risk, p.Notes, p.TradingViewPlan, p.RewardRiskRatio, p.Profit)
 	if err != nil {
 		return 0, err
 	}
@@ -20,7 +20,7 @@ func (db *Database) AddPosition(p Position) (TradeID int64, err error) {
 }
 
 func (db *Database) GetPositions() (p []Position, err error) {
-	rows, err := db.database.Query("SELECT PairID, Status, Size FROM `POSITION`;")
+	rows, err := db.database.Query("SELECT * FROM `POSITION`;")
 	if err != nil {
 		return nil, err
 	}
@@ -28,7 +28,7 @@ func (db *Database) GetPositions() (p []Position, err error) {
 
 	for rows.Next() {
 		var pos Position
-		if err := rows.Scan(&pos.PairID, &pos.Status, &pos.Size); err != nil {
+		if err := rows.Scan(&pos.PositionID, &pos.PairID, &pos.Status, &pos.Side, &pos.Risk, &pos.Notes, &pos.TradingViewPlan, &pos.RewardRiskRatio, &pos.Profit, &pos.EntryTime, &pos.ModifyTime); err != nil {
 			return nil, err
 		}
 		p = append(p, pos)

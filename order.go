efad3557e51f522f23e13e73968ada66ae47a03b
dez11de <dennis@ganzeinfach.nl>
2022-01-12 14:05:14 +0100

    Refactor Order to use decimal.Decimal instead of float64 or DOUBLE
    This prevents rounding errors.

--- order.go
+++ order.go
@@ -1,16 +1,20 @@
 package main
 
-import "time"
+import (
+	"time"
+
+	"github.com/shopspring/decimal"
+)
 
 type Order struct {
 	OrderID         int64
 	PositionID      int64
 	ExchangeOrderID string `json:"order_link_id"`
 	Status          Status
-	OrderType       OrderType `json:"order_type"`
-	Size            float64   `json:"qty"`
-	TriggerPrice    float64   `json:"tp_trigger"`
-	Price           float64   `json:"price"`
+	OrderType       OrderType       `json:"order_type"`
+	Size            decimal.Decimal `json:"qty"`
+	TriggerPrice    decimal.Decimal `json:"tp_trigger"`
+	Price           decimal.Decimal `json:"price"`
 	EntryTime       time.Time
 	ModifyTime      time.Time
 }

bc724b2f66da14336a6c150dc9fd44e0dc84bab5
dez11de <dennis@ganzeinfach.nl>
2022-01-12 13:59:57 +0100

    Refactor Pair to use decimal.Decimal instead of float64
    Prevents rounding errors

--- pair.go
+++ pair.go
@@ -1,32 +1,34 @@
 package main
 
+import "github.com/shopspring/decimal"
+
 type pairLeverage struct {
-	Min  float64 `json:"min_leverage"`
-	Max  float64 `json:"max_leverage"`
-	Step float64 `json:"leverage_step,string"`
+	Min  decimal.Decimal `json:"min_leverage"`
+	Max  decimal.Decimal `json:"max_leverage"`
+	Step decimal.Decimal `json:"leverage_step,string"`
 }
 
 type pairPrice struct {
-	Min  float64 `json:"min_price,string"`
-	Max  float64 `json:"max_price,string"`
-	Tick float64 `json:"tick_size,string"`
+	Min  decimal.Decimal `json:"min_price,string"`
+	Max  decimal.Decimal `json:"max_price,string"`
+	Tick decimal.Decimal `json:"tick_size,string"`
 }
 
 type pairOrderSize struct {
-	Min  float64 `json:"min_trading_qty"`
-	Max  float64 `json:"max_trading_qty"`
-	Step float64 `json:"qty_step"`
+	Min  decimal.Decimal `json:"min_trading_qty"`
+	Max  decimal.Decimal `json:"max_trading_qty"`
+	Step decimal.Decimal `json:"qty_step"`
 }
 
 type pair struct {
 	PairID        int64
-	Pair          string        `json:"name"`
-	BaseCurrency  string        `json:"base_currency"`
-	QuoteCurrency string        `json:"quote_currency"`
-	PriceScale    int           `json:"price_scale"`
-	TakerFee      float64       `json:"taker_fee,string"`
-	MakerFee      float64       `json:"maker_fee,string"`
-	Leverage      pairLeverage  `json:"leverage_filter"`
-	Price         pairPrice     `json:"price_filter"`
-	OrderSize     pairOrderSize `json:"lot_size_filter"`
+	Pair          string          `json:"name"`
+	BaseCurrency  string          `json:"base_currency"`
+	QuoteCurrency string          `json:"quote_currency"`
+	PriceScale    int             `json:"price_scale"`
+	TakerFee      decimal.Decimal `json:"taker_fee,string"`
+	MakerFee      decimal.Decimal `json:"maker_fee,string"`
+	Leverage      pairLeverage    `json:"leverage_filter"`
+	Price         pairPrice       `json:"price_filter"`
+	OrderSize     pairOrderSize   `json:"lot_size_filter"`
 }
